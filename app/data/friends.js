704c730b8f1b4be83be847ca238fb49bd811b054
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-11 16:45:15 -0500

    got the logic in apiRoutes to send totals to an array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,52 @@
 var friendsArray = [
     {
-        "name":"Ahmed",
+        "name":"Ross",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
           ]
-      }    
+      },
+      {
+        "name":"Mark",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      // {
+      //   "name":"Joseph",
+      //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      //   "scores":[
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1,
+      //       1
+      //     ]
+      // }          
 ]
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,66 @@
 var friendsData = require('../data/friends')
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get('/api/friends', function(req, res) {
+    app.get('/api/friends', function (req, res) {
         res.json(friendsData)
-        
+
     })
 
-    app.post('/api/friends', function(req, res) {
+    app.post('/api/friends', function (req, res) {
         var newFriend = req.body;
-
         friendsData.push(newFriend)
         res.json(newFriend)
+
+        newScores()
+
+        function newScores() {
+            var newScores = newFriend.scores;
+            var result = newScores.map(function (x) {
+                return parseInt(x, 10)
+            })
+
+            var differences = 0;
+            var differenceArray = []
+            var userResult = result
+            // var comparedResult = friendsData[0].scores
+            // console.log(comparedResult)
+            var totalDifference = [];
+
+            const add = (a, b) => a + b
+
+            if (friendsData.length > 1) {
+
+                for (x = 0; x < friendsData.length; x++) {
+
+                    var comparedResult = friendsData[x].scores
+                    // var totalDifference = 0;
+                    // console.log(comparedResult)
+
+                     if(totalDifference.length > 0) {
+                            differences = 0;
+                        }
+                        
+                
+                    for (i = 0; i < result.length; i++) {
+                        
+                        var difference = userResult[i] - comparedResult[i]
+                        differences += Math.abs(difference)
+                        console.log(differences)
+                    
+                       
+
+
+                    } 
+
+                    totalDifference.push(differences); 
+                }
+
+                console.log(totalDifference)
+            }
+
+        }
+
     })
 
 
