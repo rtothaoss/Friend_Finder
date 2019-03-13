f97ee69be8562a52519572cf7af93973023c631c
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-13 14:09:02 -0500

    finished just need to fix pictures in modal and do styling

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,71 +9,52 @@ module.exports = function (app) {
 
     app.post('/api/friends', function (req, res) {
         var newFriend = req.body;
+        var quizResults = newFriend.scores.map(function (x) {
+            return parseInt(x, 10)
+        })
 
+        var totalDifferences = [];
 
-        newScores()
+        for (x = 0; x < friendsData.length; x++) {
 
-        function newScores() {
-            var newScores = newFriend.scores;
-            var result = newScores.map(function (x) {
-                return parseInt(x, 10)
-            })
+            var friendsDataScores = friendsData[x].scores;
 
-            var differences = 0;
-            var userResult = result
+            var difference = 0;
 
-            var totalDifference = [];
+            for (i = 0; i < quizResults.length; i++) {
+                // console.log(quizResults[i] - friendsDataScores[i]);
 
-
-            if (friendsData.length > 1) {
-
-                for (x = 0; x < friendsData.length; x++) {
-
-                    var comparedResult = friendsData[x].scores
-
-
-                    if (totalDifference.length > 0) {
-                        differences = 0;
-                    }
-
-                    for (i = 0; i < result.length; i++) {
-
-                        var difference = userResult[i] - comparedResult[i]
-                        differences += Math.abs(difference)
-                        // console.log(differences)
-
-                    }
-
-                    totalDifference.push(differences);
-                }
-
-                // console.log(totalDifference)
-
-                var minVal = Math.min.apply(null, totalDifference) 
-                // console.log(minVal)
-                var indexTotDif = totalDifference.indexOf(minVal)
-
-                console.log(indexTotDif)
-                console.log(friendsData[indexTotDif].name)
-                console.log(friendsData[indexTotDif].photo)
-
-                var newFriendObj = [
-                    {
-                     name:  friendsData[indexTotDif].name
-                    },
-                    {
-                    photo: friendsData[indexTotDif].photo
-                    }
-                ]
+                difference += Math.abs(quizResults[i] - friendsDataScores[i]) // 3 5
 
             }
-            friendsData.push(newFriend)
-            res.json(newFriendObj)
-        
+
+            totalDifferences.push(difference);
         }
 
-        // friendsData.push(newFriend)
-        // res.json(friendsData[indexTotDif].name)
+        console.log(totalDifferences)
+
+        var minVal = Math.min.apply(null, totalDifferences);
+
+
+        totalDifferences[0];
+
+
+        var indexTotDif = totalDifferences.indexOf(minVal)
+
+
+        var newFriendObj = [
+            {
+                name: friendsData[indexTotDif].name
+            },
+            {
+                photo: friendsData[indexTotDif].photo
+            }
+        ]
+
+
+        friendsData.push(newFriend)
+        res.json(newFriendObj)
+
 
     })
 
