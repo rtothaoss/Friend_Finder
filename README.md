532d48b9ff9c398675d0508bef8878c4bfd9c233
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-03-14 13:47:02 -0500

    updated read me and added in a better friends array

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Friend_Finder
+
+*An app that allows the user to take a survey and find out who could be their next best friend.*
+
+Deployed Page: https://guarded-castle-93132.herokuapp.com/
+
+### Technologies Used:
+* Express.js
+* node.js
+* Bootstrap
+* jQuery
+* API and HTML Routing
+* GET/POST
+
+This project involved mating together the front end and back end to make a complete page. 
+
+The user is first taken to a landing page where they can choose to take a survey. They are then sent to a survey page where they are asked to input their name and a link to an image of themselves. After those two inputs they can take a survey of 10 questions. After the survey is completed and submitted they will be paired up with a friend from the database that most closely matches their answers. 
+
+
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var friendsArray = [
     {
-        "name":"Ross",
-        "photo":"https://s3.amazonaws.com/fixd-wordpress-cdn/wp-content/uploads/2018/04/Picture-of-person.png",
+        "name":"Michael Dell",
+        "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Michael_Dell_2010.jpg/220px-Michael_Dell_2010.jpg",
         "scores":[
             5,
+            4,
+            3,
+            2,
+            1,
             5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5
+            4,
+            3,
+            2,
+            1
           ]
       },
       {
-        "name":"Mark",
-        "photo":"https://s3.amazonaws.com/fixd-wordpress-cdn/wp-content/uploads/2018/04/Picture-of-person.png",
+        "name":"Mark Zuckerberg",
+        "photo":"https://media.wired.com/photos/592676467034dc5f91beb80e/master/pass/MarkZuckerberg.jpg",
         "scores":[
             3,
             3,
@@ -31,22 +31,24 @@ var friendsArray = [
             3
           ]
       },
-      // {
-      //   "name":"Joseph",
-      //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      //   "scores":[
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1,
-      //       1
-      //     ]
-      // }          
+      {
+        "name":"Bill Gates",
+        "photo":"https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X_400x400.jpg",
+        "scores":[
+            2,
+            4,
+            2,
+            4,
+            2,
+            3,
+            2,
+            1,
+            5,
+            1
+          ]
+      }
+
+          
 ]
 
 module.exports = friendsArray;
